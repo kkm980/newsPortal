28e242dc064cea1d9f60e464445c39bc1fc26d63
krishna kant <kantkrishna751@gmail.com>
2023-01-17 04:08:28 +0530

    checked bug for email and password check

--- news_portal_server/src/utils/passwordRange.ts
+++ news_portal_server/src/utils/passwordRange.ts
@@ -2,10 +2,10 @@ async function passwordValid(password: string) {
   //  our function to check for specific password range, length and characters,
   // for now have kept it simple for test purpose
   if(password!=""){
-    return true;
+    return false;
   }
   else{
-    return false;
+    return true;
   }
 };
 
